be01deb9e46e7bb9541f272ac9227eb2031bb5c3
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-07 14:55:02 +0200

    Initial Commit

---
+++ FormValidationMvApproach/FormValidationMvApproachApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormValidationMvApproachApp.swift
+//  FormValidationMvApproach
+//
+//  Created by Buhle Radzilani on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct FormValidationMvApproachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
