e5948c6ccc2b6dcffb7f2d68e03523a7850d8734
buhleradzilani <radzilanibuhle@gmail.com>
2024-06-07 16:21:39 +0200

    Adding the LoginFormError struct which will contain all the messages that will be displayed if the user enters the wrong email or password.

--- FormValidationMvApproach/ContentView.swift
+++ FormValidationMvApproach/ContentView.swift
@@ -17,21 +17,52 @@ struct ContentView: View {
     @State private var email: String = ""
     @State private var password: String = ""
     //Login form error instance
-    @State private var loginFormError = LoginFormError() 
+    @State private var loginFormError = LoginFormError()
+    
+    private func clearForm() {
+        loginFormError = LoginFormError()
+    }
     
     var isFormValid: Bool{
-        return !email.isEmpty && !password.isEmpty && email.isValidEmail
+        
+        clearForm()
+        
+        if email.isEmpty{
+            loginFormError.email = "Email is required!"
+        }else if !email.isValidEmail {
+            loginFormError.email = "Email is not in correct format!"
+        }
+        
+        if password.isEmpty{
+            loginFormError.password = "Password is required!"
+        }
+        
+        return loginFormError.email.isEmpty && loginFormError.password.isEmpty
+      
     }
     
     var body: some View {
         Form{
             TextField("Email", text: $email)
                 .textInputAutocapitalization(.never)
-            SecureField("Password", text: $password)
-            Button("Login"){
-                
-            }.disabled(!isFormValid)
+            if !loginFormError.email.isEmpty{
+                Text(loginFormError.email)
+                    .font(.caption)
+                    .foregroundColor(.red)
+            }
             
+            SecureField("Password", text: $password)
+            if !loginFormError.password.isEmpty{
+                Text(loginFormError.password)
+                    .font(.caption)
+                    .foregroundColor(.red)
+            }
+          
+            Button("Login"){
+                if isFormValid{
+                    
+                }
+            }
         }
     }
 }
